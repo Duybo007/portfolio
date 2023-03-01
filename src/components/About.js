b81a7b848f68100bd2e8f66276bbd3b7d7af2daa
duybo <duybo95@gmail.com>
2023-03-01 12:18:23 -0800

    remove dall e

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ function About() {
 
   return (
     <motion.div
-                initial={{opacity: 0,}}
-                whileInView={{opacity : 1}}
+                initial={{ x: 200, opacity: 0 }}
+                whileInView={{opacity : 1, x : 0 }}
                 transition={{duration: 1.5}}
                 viewport={{ once: true }}
                 className='about'>
